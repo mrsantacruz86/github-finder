3a5e3f4328991a706cadcfbd9e8a8b6201e12e13
Nelson Diaz <abnerdc86@gmail.com>
2025-09-21 03:51:50 -0400

    Add search suggestions feature

--- src/api/github.ts
+++ src/api/github.ts
@@ -6,6 +6,19 @@ export const fetchGithubUser = async (username: string) => {
   if (!res.ok) throw new Error('User not found');
 
   const data = await res.json();
-  console.log(data);
   return data;
+};
+
+export const searchGithubUser = async (
+  query: string,
+  numberOfUsers: number
+) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}`
+  );
+
+  if (!res.ok) throw new Error('User not found');
+
+  const data = await res.json();
+  return data.items.slice(0, numberOfUsers);
 };
